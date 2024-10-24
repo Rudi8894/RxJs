633022cf0f327bc27fa6ab0d5c666cc559556415
Rudi8894 <rudi.almouhammed@gmx.ch>
2024-10-24 14:59:31 +0200

    asdsa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ObservablesComponent } from './observables/observables.component';
+import { OperatorsComponent } from './operators/operators.component';
 
 export const routes: Routes = [
     {
         path:'',
-        component: ObservablesComponent
+        component: OperatorsComponent
     }
 ];
--- src/app/operators/operators.component.ts
+++ src/app/operators/operators.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter, from, map, of, Subscription, take, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-operators',
@@ -7,6 +8,56 @@ import { Component } from '@angular/core';
   templateUrl: './operators.component.html',
   styleUrl: './operators.component.css'
 })
-export class OperatorsComponent {
+export class OperatorsComponent implements OnInit {
+  subApple!: Subscription
+ ngOnInit(): void {
+  //  of(2,6,4,5)
+  //   .pipe(
+  //     map(i=> i *3),
+  //     tap(i=> console.log(i)),
+  //     take(3)
+  //   ).subscribe(i=> console.log(i));
+
+    of(2,4,6)
+      .pipe(
+        map(i=> i * 3),
+        map(i=> i + 5)
+      ).subscribe(i=> console.log(i));
+
+    of(2,4,6)
+      .pipe(
+        map(i=>{
+          i = i *3;
+          return i+ 5;
+        })
+      ).subscribe(i=> console.log(i));
+      const apple$ = from([
+        {id: 1, type:'macintosch'},
+        {id: 2, type:'gala'},
+        {id: 3, type:'fuji'},
+      ]);
+      this.subApple = apple$.pipe(
+          map(a=> {
+            return {...a, color: 'red'}
+          })
+      ).subscribe(a=> console.log(a));
+
+      of(2,4,5,6).pipe(
+        filter(f=> f % 2===0),
+        tap(i=> console.log(i))
+      ).subscribe();
+
+      // start with number 0 and warit for 1 second to emmit the next item
+      timer(0,1000).pipe(
+        take(4)
+      ).subscribe({
+        next: i=> console.log(i),
+        error: err=> console.log(err),
+        complete: ()=> console.log('completed')
+      });
+
+ }
+
+ 
 
 }
